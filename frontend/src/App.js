e80194ddb1ff463c44067c35fee8590da9540813
Mario Vogel <mariovogel2000@gmail.com>
2025-05-05 14:36:20 +0200

    add material design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
@@ -7,17 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CO2 footprint
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <Button>test</Button>
     </div>
   );
 }
