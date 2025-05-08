5c69627725d2453adac4540bede4be8f0f9b765d
Mario Vogel <mariovogel2000@gmail.com>
2025-05-08 09:50:08 +0200

    filter implementation

--- frontend/src/components/CustomTable.js
+++ frontend/src/components/CustomTable.js
@@ -1,28 +1,33 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, QuickFilter } from '@mui/x-data-grid';
 import { Paper } from '@mui/material';
+import { useState } from 'react';
 
-function CustomTable(data) {
+function CustomTable(data, filterBy) {
 
     const columns = Object.keys(data.data[0]).map((key) => ({
         field: key, headerName: key, flex: 1
     }));
 
-    console.log(data.data)
-
     const paginationModel = { page: 0, pageSize: 20 };
+
+    const [filterModel, setFilterModel] = useState({ items: [] })
 
     return (
         <Paper sx={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={data.data}
-          columns={columns}
-          initialState={{ pagination: { paginationModel } }}
-          pageSizeOptions={[20, 50, { value: -1, label: 'Alle' }]}
-          checkboxSelection
-          sx={{ border: 0 }}
-          getRowId={(row) => row.Name}
-        />
-      </Paper>
+            <QuickFilter>
+</QuickFilter>;
+            <DataGrid
+                rows={data.data}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[20, 50, { value: -1, label: 'Alle' }]}
+                checkboxSelection
+                sx={{ border: 0 }}
+                getRowId={(row) => row.Name}
+                filterModel={filterModel}
+                onFilterModelChange={() => setFilterModel({ items: [{ field: data.filterBy, operator: 'equals', value: 'Land' }] })}
+            />
+        </Paper>
     )
 }
 
