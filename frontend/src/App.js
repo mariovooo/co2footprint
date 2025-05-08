4c0e3445fc74d741a077f50b98f5b5ecebc737b4
Mario Vogel <mariovogel2000@gmail.com>
2025-05-08 16:16:39 +0200

    drawer for anchor navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,108 +11,112 @@ import RecyclingIcon from '@mui/icons-material/Recycling';
 import CategoryIcon from '@mui/icons-material/Category';
 import theme from './theme';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import CustomDrawer from './components/CustomDrawer';
 
 function App() {
-  
+
   return (
     <ThemeProvider theme={theme}>
-    <div className="">
-      <AppBar className="" position="sticky">
-        <Toolbar>
-          <div className="headerFlex">
-            <Box className="headerFlexInner">
-              <img src={logo} className="headerLogo" alt="logo" />
-              <Typography variant="h5" component="div">
-                CO2-Footprint
-              </Typography>
-            </Box>
-            <Box className="headerFlexInner">
-              <Button variant="contained" disableElevation>Startseite</Button>
-              <Button variant="contained" disableElevation>Einstellungen</Button>
-              <Button variant="contained" disableElevation>Abmelden</Button>
-            </Box>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <section id="content">
-        <Grid container spacing={10}>
-          <Container>
-            <div className="text">
-              <h2>Ausstoß (in Millionen Tonnen CO2)</h2>
-              <CustomTable data={Emissionen} />
+      <div className="">
+        <AppBar className="" position="sticky">
+          <Toolbar>
+            <div className="headerFlex">
+              <Box className="headerFlexInner">
+                <img src={logo} className="headerLogo" alt="logo" />
+                <Typography variant="h5" component="div">
+                  CO2-Footprint
+                </Typography>
+              </Box>
+              <Box className="headerFlexInner">
+                <Button variant="contained" disableElevation>Startseite</Button>
+                <Button variant="contained" disableElevation>Einstellungen</Button>
+                <Button variant="contained" disableElevation>Abmelden</Button>
+              </Box>
             </div>
-          </Container>
+          </Toolbar>
+        </AppBar>
+        <section id="content">
 
-          <Grid container spacing={4}>
-            <Grid size={6}>
-              <Box
-                component="img"
-                sx={{
-                  width: "100%",
-                  maxHeight: { xs: 480, md: 720 },
-                  maxWidth: { xs: 720, md: 1280 },
-                }}
-                alt="Green field"
-                title="Photo: Myrabella / Wikimedia Commons"
-                src="./field.jpg"
-              />
-            </Grid>
-            <Grid size={6}>
-              <Container>
+          <CustomDrawer />
+
+          <Grid container spacing={10}>
+            <Container>
               <div className="text">
-                <h2>Was ist der CO2-Fußabdruck?</h2>
-                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                </p>
+                <h2>Ausstoß (in Millionen Tonnen CO2)</h2>
+                <CustomTable data={Emissionen} />
               </div>
-              </Container>
-            </Grid>
-          </Grid>
+            </Container>
 
-          <Container>
-            <h2>Sektoren</h2>
             <Grid container spacing={4}>
-              <Grid size={4}>
-                <CustomCard title="Energie" text="Dies ist ein informativer Text innerhalb der Card." icon={BoltIcon} />
+              <Grid size={6}>
+                <Box
+                  component="img"
+                  sx={{
+                    width: "100%",
+                    maxHeight: { xs: 480, md: 720 },
+                    maxWidth: { xs: 720, md: 1280 },
+                  }}
+                  alt="Green field"
+                  title="Photo: Myrabella / Wikimedia Commons"
+                  src="./field.jpg"
+                />
               </Grid>
-              <Grid size={4}>
-                <CustomCard title="Landwirtschaft" text="Dies ist ein informativer Text innerhalb der Card." icon={AgricultureIcon} />
-              </Grid>
-              <Grid size={4}>
-                <CustomCard title="Industrie" text="Dies ist ein informativer Text innerhalb der Card." icon={FactoryIcon} />
-              </Grid>
-              <Grid size={4}>
-                <CustomCard title="Abfall" text="Dies ist ein informativer Text innerhalb der Card." icon={RecyclingIcon} />
-              </Grid>
-              <Grid size={4}>
-                <CustomCard title="Sonstige Sektoren" text="Dies ist ein informativer Text innerhalb der Card." icon={CategoryIcon} />
+              <Grid size={6}>
+                <Container>
+                  <div className="text">
+                    <h2>Was ist der CO2-Fußabdruck?</h2>
+                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    </p>
+                  </div>
+                </Container>
               </Grid>
             </Grid>
+
+            <Container>
+              <h2>Sektoren</h2>
+              <Grid container spacing={4}>
+                <Grid size={4}>
+                  <CustomCard title="Energie" text="Dies ist ein informativer Text innerhalb der Card." icon={BoltIcon} />
+                </Grid>
+                <Grid size={4}>
+                  <CustomCard title="Landwirtschaft" text="Dies ist ein informativer Text innerhalb der Card." icon={AgricultureIcon} />
+                </Grid>
+                <Grid size={4}>
+                  <CustomCard title="Industrie" text="Dies ist ein informativer Text innerhalb der Card." icon={FactoryIcon} />
+                </Grid>
+                <Grid size={4}>
+                  <CustomCard title="Abfall" text="Dies ist ein informativer Text innerhalb der Card." icon={RecyclingIcon} />
+                </Grid>
+                <Grid size={4}>
+                  <CustomCard title="Sonstige Sektoren" text="Dies ist ein informativer Text innerhalb der Card." icon={CategoryIcon} />
+                </Grid>
+              </Grid>
             </Container>
             <Container>
 
 
 
-          <div className="text">
-            <h2>Überschrift3</h2>
-            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-          </div>
-          <div className="text">
-            <h2>Überschrift4</h2>
-            <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-          <div className="text">
-            <h2>Überschrift5</h2>
-            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-          </div>
-          </Container>
-        </Grid>
-      </section>
-    </div>
-    <CssBaseline />
-  </ThemeProvider>
+              <div className="text">
+                <h2>Überschrift3</h2>
+                <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+              </div>
+              <div className="text">
+                <h2>Überschrift4</h2>
+                <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div className="text">
+                <h2>Überschrift5</h2>
+                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+              </div>
+            </Container>
+          </Grid>
+        </section>
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/components/CustomDrawer.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import Fab from '@mui/material/Fab';
+import { useState } from 'react';
+
+function CustomDrawer() {
+
+    function StringToSlug(text) {
+        return text
+            .toLowerCase()
+            .trim()
+            .replace(/[^\w\s-]/g, '')
+            .replace(/\s+/g, '-')
+            .replace(/-+/g, '-');
+    }
+
+    const h2Elements = document.querySelectorAll('h2');
+    const h2Array = Array.from(h2Elements).map(h2 => {
+        let id = h2.id;
+        if (!id) {
+            id = StringToSlug(h2.textContent.trim());
+            h2.setAttribute("id", id);
+        }
+        return [h2.textContent.trim(), `#${id}`];
+    });
+
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+            <List>
+                {h2Array.map((h2) => (
+                    <ListItem key={h2[0]} disablePadding>
+                        <ListItemButton component="a" href={h2[1]}>
+                            <ListItemText primary={h2[0]} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    return (
+        <>
+            <Fab variant="extended" onClick={toggleDrawer(true)} sx={{
+                position: 'fixed',
+                bottom: 16,
+                left: 16,
+                zIndex: 1000,
+            }}>
+                <NavigationIcon sx={{ mr: 1 }} />
+                Navigation
+            </Fab>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+        </>
+
+    )
+}
+
+export default CustomDrawer
