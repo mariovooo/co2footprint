967308e8b19bd4837b117b1a86eed5d5feda08f1
Mario Vogel <mariovogel2000@gmail.com>
2025-05-08 10:02:48 +0200

    different filter approach

--- frontend/src/components/CustomTable.js
+++ frontend/src/components/CustomTable.js
@@ -1,8 +1,7 @@
 import { DataGrid, QuickFilter } from '@mui/x-data-grid';
 import { Paper } from '@mui/material';
-import { useState } from 'react';
 
-function CustomTable(data, filterBy) {
+function CustomTable(data) {
 
     const columns = Object.keys(data.data[0]).map((key) => ({
         field: key, headerName: key, flex: 1
@@ -10,22 +9,20 @@ function CustomTable(data, filterBy) {
 
     const paginationModel = { page: 0, pageSize: 20 };
 
-    const [filterModel, setFilterModel] = useState({ items: [] })
-
     return (
         <Paper sx={{ height: 400, width: '100%' }}>
-            <QuickFilter>
-</QuickFilter>;
+
             <DataGrid
                 rows={data.data}
                 columns={columns}
-                initialState={{ pagination: { paginationModel } }}
+                initialState={{
+                    pagination: { paginationModel }
+                }}
                 pageSizeOptions={[20, 50, { value: -1, label: 'Alle' }]}
                 checkboxSelection
                 sx={{ border: 0 }}
                 getRowId={(row) => row.Name}
-                filterModel={filterModel}
-                onFilterModelChange={() => setFilterModel({ items: [{ field: data.filterBy, operator: 'equals', value: 'Land' }] })}
+                showToolbar
             />
         </Paper>
     )
