1f3922e39f3106a80006a423cf05b56c039b9662
Mario Vogel <mariovogel2000@gmail.com>
2025-05-09 14:41:16 +0200

    bugfix scrolling behavior

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import '@fontsource/josefin-sans/300.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import theme from './theme';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import CustomDrawer from './components/CustomDrawer';
@@ -9,12 +9,23 @@ import About from './pages/About';
 import Participate from './pages/Participate';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useEffect } from 'react';
 
 function App() {
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
   return (
     <Router>
-
+      <ScrollToTop />
       <ThemeProvider theme={theme}>
         <div className="">
           <Header />
